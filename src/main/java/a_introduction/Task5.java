f996c3def2b1ae84469dd3a46615f703e8495698
lidiamiskodpd <lidia.misko@dpdgroup.com>
2023-04-20 18:57:49 +0200

    Zadania Szkolenie

--- src/main/java/a_introduction/Task5.java
+++ src/main/java/a_introduction/Task5.java
@@ -3,5 +3,23 @@ package a_introduction;
 public class Task5 {
     public static void main(String[] args) {
 
+        String imie = "Jan";
+        int wiek = 10;
+
+        if (wiek >= 18) {
+            System.out.println(imie + " ma " + wiek + " i jest pełnoletni");
+        }
+            else {
+                System.out.println(imie + " ma " + wiek + " i nie jest pełnoletni");
+            }
+
+        }
     }
-}
+
+
+
+//Stwórz dwie zmienne:
+//- String imie
+//- int wiek
+//
+//Wydrkuj napis w postaci "Imię, ma x lat i jest/nie jest pełnoletni" w zależności od podanego parametru wieku.
--- src/main/java/a_introduction/Task6.java
+++ src/main/java/a_introduction/Task6.java
@@ -2,6 +2,27 @@ package a_introduction;
 
 public class Task6 {
     public static void main(String[] args) {
+        double alcoholContent = 5.5;
+        final String beerType = alcoholContent >= 4
+                ? "mocne"
+            :"słabe";
+        System.out.println(beerType);
+    }}
 
-    }
-}
+
+
+
+
+
+//Użyj ternary operatora żeby zapisać krócej:
+//
+//```java
+//double alcoholContent = 5.5;
+//String beerType;
+//if (alcoholContent >= 4) {
+//    beerType = "mocne";
+//}
+//else {
+//    beerType = "słabe";
+//}
+//```
