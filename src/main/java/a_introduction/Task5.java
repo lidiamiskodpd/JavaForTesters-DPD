15d571546f35f9719eb1d5938c9b3fae1f5a9fe6
lidiamiskodpd <lidia.misko@dpdgroup.com>
2023-04-20 19:04:33 +0200

    Zadania Szkolenie

--- src/main/java/a_introduction/Task5.java
+++ src/main/java/a_introduction/Task5.java
@@ -1,10 +1,17 @@
 package a_introduction;
 
+import java.util.Scanner;
+
 public class Task5 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imię");
+    String imie = scanner.nextLine();
 
-        String imie = "Jan";
-        int wiek = 10;
+
+       // int wiek = 10;
+        System.out.println("Podaj wiek");
+        int wiek = scanner.nextInt();
 
         if (wiek >= 18) {
             System.out.println(imie + " ma " + wiek + " i jest pełnoletni");
